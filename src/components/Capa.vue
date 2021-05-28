3b0ba6f56c86d6804b9f7046d5612914b4e31ed8
alan.ferreira@t2mlab.com <alan.ferreira@t2mlab.com>
2021-05-28 19:52:18 -0300

    Arquitetura landing page

---
+++ src/components/Capa.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="capa">
+    <div class="textos__capa">
+      <div class="elementos__texto">
+        <div class="titulo__capa">
+          <h1>We Believe in Great Architeture</h1>
+        </div>
+        <div class="descricao__capa">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque nobis
+            repellat dolorem quam corrupti tenetur laboriosam repellendus
+            incidunt quas, nemo, in facere! Culpa, harum expedita at voluptates
+            cumque eos cupiditate.
+          </p>
+        </div>
+        <button class="btn__projetos">Projetos</button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.capa {
+  height: calc(100vh - 70px);
+  background: url(https://images.pexels.com/photos/1758672/pexels-photo-1758672.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260)
+    no-repeat;
+  background-size: cover;
+  background-attachment: fixed;
+}
+.elementos__texto {
+  max-width: 80%;
+}
+.textos__capa {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 1em;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  color: white;
+  transition: 0.2s ease-in;
+}
+.titulo__capa {
+margin: 2em 0;
+}
+.titulo__capa h1{
+    font-size: 4em;
+    font-weight: 300;
+    transition: .2s ease-in;
+}
+.descricao__capa{
+    margin: 2em 0;
+}
+.descricao__capa p{
+    font-weight: 300;
+}
+.btn__projetos{
+    padding: 1em 2.5em;
+    color: white;
+    background: var(--buton-capa-color);
+    border: none;
+    transition: .2s ease;   
+}
+.btn__projetos:hover{
+    background: var(--buton-capa-color--hover);
+}
+
+@media screen and (max-width: 700px) {
+  .textos__capa {
+    max-width: 100%;
+  }
+}
+
+@media screen and (max-width: 500px) {
+ .titulo__capa h1{
+     font-size: 2.5rem;
+ }
+ .descricao__capa p{
+     text-align: justify;
+     font-size: .8em;
+ }
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,157 @@
+<template>
+  <header>
+    <nav>
+      <div class="logo">
+        <span>LOGO.CO</span>
+      </div>
+      <ul class="links">
+        <li><a href="">Home</a></li>
+        <li><a href="">Sobre</a></li>
+        <li><a href="">Projetos</a></li>
+        <li><a href="">Fale conosco</a></li>
+      </ul>
+      <button class="fancy-burger" @click="nav">
+        <span class="box"></span>
+        <span class="rectangle rectangle--top rectangle--small"></span>
+        <span class="rectangle rectangle--middle"></span>
+        <span class="rectangle rectangle--bottom rectangle--small"></span>
+      </button>
+    </nav>
+  </header>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      active: false,
+      active2: false,
+    };
+  },
+  methods: {
+    closeNavbar() {},
+
+    nav() {
+      let btnHamburguer = document.querySelector(".fancy-burger");
+      let overlay = document.querySelector(".links");
+
+      this.active == true ? (this.active = false) : (this.active = true);
+
+      btnHamburguer.querySelectorAll("span").forEach((span) => {
+          span.classList.toggle("open");
+      });
+
+      this.active ? overlay.style.left = 0 : overlay.style.left = '-100%'
+    },
+  },
+};
+</script>
+<style scoped>
+header {
+  width: 100%;
+  height: 70px;
+  padding: 1em;
+}
+nav {
+  height: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+.logo span{
+    font-weight: 700;
+}
+.fancy-burger{
+ display: none;
+  position: relative;
+  width: var(--button-width);
+  height: var(--button-height);
+  border: 0;
+  background: transparent;
+  outline: none;
+  cursor: pointer;
+}
+.rectangle {
+  position: absolute;
+  height: var(--rectangle-height);
+  background: black;
+  border-radius: var(--rectangle-radius);
+  transition: transform var(--transition);
+}
+.rectangle--small {
+  width: calc(var(--button-width) / 2);
+}
+.rectangle--top {
+  top: 0;
+  left: 0;
+  transform-origin: top left;
+}
+.rectangle--top.open {
+  transform: translateX(var(--translate)) rotate(45deg);
+}
+.rectangle--middle {
+  top: 50%;
+  left: 0;
+  width: var(--button-width);
+  transform: translatey(-50%);
+}
+.rectangle--middle.open {
+  transform: translatey(-50%) rotate(-45deg);
+}
+.rectangle--bottom {
+  bottom: 0;
+  right: 0;
+  transform-origin: bottom right;
+}
+.rectangle--bottom.open {
+  transform: translateX(calc(-1 * var(--translate))) rotate(45deg);
+}
+
+.links li {
+  display: inline-block;
+  margin: 0 0.5em;
+ 
+}
+.links li a {
+  text-decoration: none;
+   padding: .5em;
+   color: black;
+   transition: .1s ease-in;
+}
+.links li a:hover{
+    background: var(--buton-capa-color);
+    color: white;
+}
+
+.bars {
+  display: none;
+  cursor: pointer;
+}
+
+@media screen and (max-width: 600px) {
+  .fancy-burger{
+      display: block;
+  }
+  .links {
+    position: fixed;
+    width: 70%;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    top: 0;
+    left: -100%;
+    text-align: center;
+    transition: all 0.5s;
+    background: rgba(0, 0, 0, 0.93);
+  }
+  .links li {
+    margin: 2em 0;
+  }
+  .links li a {
+    color: white;
+  }
+}
+</style>
